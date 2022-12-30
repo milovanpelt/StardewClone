57263de49751e2e15a3bf85d8471136ccbefbb6a
191484 <191484@buas.nl>
2022-12-30 18:02:44 +0100

    [CHANGED] Tile creation happens in Awake now

--- Assets/Scripts/TileSpawnScript.cs
+++ Assets/Scripts/TileSpawnScript.cs
@@ -8,17 +8,22 @@ public class TileSpawnScript : MonoBehaviour
     public int rows = 0;
     public GameObject tileObject;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        for (int x = 0; x < rows; x++) 
+        for (int x = 0; x < rows; x++)
         {
-            for (int y = 0; y < colums; y++) 
+            for (int y = 0; y < colums; y++)
             {
                 Vector3 position = new Vector3(x + transform.position.x, y - 2, transform.position.z);
                 Instantiate(tileObject, position, transform.rotation);
             }
         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
