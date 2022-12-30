c631735edcea2de78e42855c198e8d0dfbff0594
191484 <191484@buas.nl>
2022-12-30 21:05:32 +0100

    [ADDED] Seeds can now be detected when clicking on them

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class LogicScript : MonoBehaviour
 {
     public Text dayText;
     private int dayCount = 0;
     private GameObject[] seedTags;
+    private SeedSpawnScript seedspawner;
 
     void Start()
     {
         seedTags = GameObject.FindGameObjectsWithTag("Seed");
+        seedspawner = GameObject.FindGameObjectWithTag("SeedSpawner").GetComponent<SeedSpawnScript>();
     }
     void Update()
     {
@@ -24,6 +25,18 @@ public class LogicScript : MonoBehaviour
             foreach (GameObject item in seedTags)
             {
                 item.GetComponent<SeedStageScript>().SetSprite(dayCount);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
             }
         }
     }
