01f7f73d3eb3181993ab2604d6bc0716887a973d
191484 <191484@buas.nl>
2023-01-10 12:16:51 +0100

    [ADDED] Seed gets destroyed when clicking on them

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -19,9 +19,11 @@ public class LogicScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            // Increase the day when pressing spacebar
             dayCount++;
             dayText.text = dayCount.ToString();
 
+            // Set seed stage equal to current day
             foreach (GameObject item in seedTags)
             {
                 item.GetComponent<SeedStageScript>().SetSprite(dayCount);
@@ -30,13 +32,16 @@ public class LogicScript : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            // Ray cast from mouse to screen in 2D
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            // Destroy seed when hit
             if (hit.collider != null)
             {
-                Debug.Log(hit.collider.gameObject.name);
+                Destroy(hit.collider.gameObject);
             }
         }
     }
