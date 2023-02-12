47f8c79151c432273dec3ae33889b46ee97baab8
191484 <191484@buas.nl>
2023-02-12 21:40:46 +0100

    [CHANGED] Set-up for each stage having its own grow day and sprite

--- Assets/Scripts/SeedStageScript.cs
+++ Assets/Scripts/SeedStageScript.cs
@@ -2,15 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct SeedStage
+{
+    public Sprite seedSprite;
+    public int GrowDays;
+}
+
 public class SeedStageScript : MonoBehaviour
 {
-    public Sprite[] spriteStages;
-    public int totalGrowDays = 0;
+    public SeedStage[] seedStage;
     private int currentStageID = 0;
+    private int totalGrowDay = 0;
+    private int lastSprite = 0;
 
     void Start()
     {
         SetSprite(currentStageID);
+
+        // Set totalGrowDay for each seed
+        for (int i = 0; i < seedStage.Length; i++)
+        {
+            totalGrowDay += seedStage[i].GrowDays;
+        }
+
+        lastSprite = seedStage.Length - 1;
     }
 
     public void NextStage() 
@@ -21,16 +37,15 @@ public class SeedStageScript : MonoBehaviour
 
     private void SetSprite(int value) 
     {
-        int lastSprite = spriteStages.Length - 1;
         if (value < lastSprite)
         {
             currentStageID = value;
-            gameObject.GetComponent<SpriteRenderer>().sprite = spriteStages[currentStageID];
+            gameObject.GetComponent<SpriteRenderer>().sprite = seedStage[currentStageID].seedSprite;
         }
-        else if (value == totalGrowDays)
+        else if (value == totalGrowDay)
         {
             currentStageID = lastSprite;
-            gameObject.GetComponent<SpriteRenderer>().sprite = spriteStages[currentStageID];
+            gameObject.GetComponent<SpriteRenderer>().sprite = seedStage[currentStageID].seedSprite;
         }
     }
 }
